dbf78088b96ac80a2ddecde08274f5fc5c89f24c
Yingxiong <liyingxiong@live.com>
2016-04-27 15:39:13 +0200

    mninor changes

--- screenmix/cross_section_view/aview.py
+++ screenmix/cross_section_view/aview.py
@@ -7,30 +7,27 @@ from abc import abstractmethod
 
 
 class AView(object):
-        
+
     @abstractmethod
-    def set_height(self,value):
+    def set_height(self, value):
         pass
-    
+
     @abstractmethod
     def set_width(self, value):
         pass
-    
+
     @abstractmethod
     def set_percent(self, value):
         pass
-    
+
     @abstractmethod
-    def add_layer(self, percent,name):
+    def add_layer(self, percent, name):
         pass
-    
+
     @abstractmethod
     def delete_layer(self):
         pass
-    
+
     @abstractmethod
-    def update_layer_information(self,name,price,density,stiffness,strength,percent):
+    def update_layer_information(self, name, price, density, stiffness, strength, percent):
         pass
-    
-        
-        
