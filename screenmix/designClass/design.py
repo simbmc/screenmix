415bb5993a813c74a024cc8796074f395753664e
mkennert <mkennert@admarc-PC>
2016-05-02 11:43:17 +0200

    -Design class -main_window rename to main

---
+++ screenmix/designClass/design.py
@@ -0,0 +1,12 @@
+'''
+Created on 29.04.2016
+
+@author: mkennert
+'''
+
+'''
+the class design contains just attributes for the design
+'''
+class Design:
+    btnSize=50
+    focusColor=[255,255,255]
